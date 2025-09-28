b2cc561fdde6e83be7bc0eaab495f29607f30021
Felipe Pinheiro <88994655+felipepinheirogh@users.noreply.github.com>
2025-09-27 21:17:19 -0300

    Update app.js

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -141,7 +141,7 @@ if (token) {
           // Alterna logo
           const logo = document.querySelector("#mainNavbar .navbar-brand img");
           if (logo) {
-            logo.src = newTheme === "dark" ? "/public/img/Gestao360-logo-white.svg" : "/public/img/Gestao360-logo-dark.svg";
+            logo.src = newTheme === "dark" ? "/public/img/Gestao360-logo-claro.svg" : "/public/img/Gestao360-logo-escuro.svg";
           }
         });
       }
